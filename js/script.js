80bb756fb7587c9390f4ac69b1eaa28b061b8464
samueldobi <frankdobisamuel@gmail.com>
2023-01-08 20:03:21 +0100

    pulled remote repository

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var search = document.querySelector('search-btn')
+var temperature = document.querySelector('temperature')
+var location = document.querySelector('location')
+var condition = document.querySelector('condition')
+//functions
+condition.addEventListener('click', ()=> {
+    console.log('hi')
+})
