6c93f4d2564c15e010bd1234d4c048db68d2b6ff
samueldobi <frankdobisamuel@gmail.com>
2023-01-12 15:45:04 +0100

    changed the font

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var search = document.querySelector('search-btn')
-var temperature = document.querySelector('temperature')
-var location = document.querySelector('location')
-var condition = document.querySelector('condition')
-//functions
-condition.addEventListener('click', ()=> {
-    console.log('hi')
-})
+// var search = document.querySelector('search-btn')
+// var temperature = document.querySelector('temperature')
+// var location = document.querySelector('location')
+// var condition = document.querySelector('condition')
+// //functions
+// condition.addEventListener('click', ()=> {
+//     console.log('hi')
+// })
