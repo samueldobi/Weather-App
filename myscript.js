4e4e8126247663273df36e1d3e4c289745f4b146
samueldobi <frankdobisamuel@gmail.com>
2023-01-14 13:47:49 +0100

    Adding more weather details

--- myscript.js
+++ myscript.js
@@ -3,6 +3,10 @@ const locate = document.querySelector('.location')
 const condition = document.querySelector('.condit')
 const searchBtn =  document.querySelector('.search-btn')
 const inputField = document.querySelector('.form-control')
+const humidity = document.querySelector('.humid')
+const pressure = document.querySelector('.pressure')
+const seaLevel = document.querySelector('.sea-level')
+const GroundLevel  = document.querySelector('.ground-level')
 
 
 searchBtn.addEventListener('click',()=>{
@@ -14,20 +18,25 @@ function getApi(city){
     fetch(api)
     .then(response => response.json())
     .then(result => (weatherDetails(result)))
-    // .catch((error) => {
-    //     console.error('Error:', error);
-    //   });
-  
 }
 function weatherDetails(info){
     if(info.cod === "404" ){
         alert('City not found')
     }
+    console.log(info)
     let degree = ((info.main.feels_like) - 273.15).toFixed(2)
     let cityName = info.name
     let weatherDescription = info.weather[0].description
-    console.log(weatherDescription)
+    let hum = info.main.humidity
+    let press = info.main.pressure
+    let sea = info.main.sea_level
+    let grnd = info.main.grnd_level
+    // console.log(weatherDescription)
     temp.innerHTML = degree
     locate.innerHTML = cityName
     condition.innerHTML = weatherDescription
+    humidity.innerHTML = hum
+    pressure.innerHTML = press
+    seaLevel.innerHTML = sea
+    GroundLevel.innerHTML = grnd
 }
